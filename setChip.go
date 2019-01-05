8ea60c45fb919acf90cbd662dfc487d4ccaa38c1
liderako <trotillostudio@gmail.com>
2019-01-05 11:35:00 +0000

    Update setChip.go

--- setChip.go
+++ setChip.go
@@ -1,6 +1,6 @@
-package main
+// package main
 
-import "fmt"
+// import "fmt"
 
 func setChip(maps [19][19]int, coordinates[]int, status[]int) [19][19] int {
 	i := 0
@@ -14,10 +14,10 @@ func setChip(maps [19][19]int, coordinates[]int, status[]int) [19][19] int {
 	return maps
 }
 
-func main() {
-	board := [19][19]int{}
-	coordinates := []int{0,0,0,1,0,2,0,3}
-	status := []int {3,3,3,3}
-	board = setChip(board, coordinates, status)
-	fmt.Println(board)
-}
+// func main() {
+// 	board := [19][19]int{}
+// 	coordinates := []int{0,0,0,1,0,2,0,3}
+// 	status := []int {3,3,3,3}
+// 	board = setChip(board, coordinates, status)
+// 	fmt.Println(board)
+// }
