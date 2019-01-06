7e202379be079b002f3ae732c4c95d466003af6a
Alex Pashkov <alexpashkov123@gmail.com>
2019-01-06 14:40:46 +0200

    Fix PossibleMoves

--- minimax/possible_moves.go
+++ minimax/possible_moves.go
@@ -18,14 +18,19 @@ func isWithinTwoFromOccupiedCell(b board.Board, c board.Coords) bool {
 	return false
 }
 
-func PossibleMoves(b board.Board, player int8) map[board.Coords]bool {
+func PossibleMoves(b board.Board) map[board.Coords]bool {
 	moves := make(map[board.Coords]bool)
 	if b == (board.Board{}) {
 		// Suggest to place into the center of the board if it's empty
 		moves[board.Coords{X: 10, Y: 10}] = true
 	} else {
+		// Go through all cells and add those that are empty and withing 2 cells
+		// from occupied to the set of possible moves
+		// In golang, set data structures are implemented as map[key]bool
 		b.ForEach(func(cell int8, coords board.Coords) {
-			moves[coords] = true
+			if b.CellIsEmpty(coords) && isWithinTwoFromOccupiedCell(b, coords) {
+				moves[coords] = true
+			}
 		})
 	}
 	return moves
