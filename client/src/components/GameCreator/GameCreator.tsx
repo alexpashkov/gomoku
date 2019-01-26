43f70311c7c2f6bdd32d5f7deb6355ce8ebb642d
Alexander Pashkov <alexpashkov123@gmail.com>
2019-01-26 15:44:44 +0200

    Add GameType enum and pass route params to the Game

--- client/src/components/GameCreator/GameCreator.tsx
+++ client/src/components/GameCreator/GameCreator.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { GameType } from "../Game/Game";
 
 const GameCreator: React.FunctionComponent<{}> = function() {
   const gameId = Date.now() + "" + Math.floor(Math.random() * 1e10);
   return (
     <React.Fragment>
-      <Link to={`/game/${gameId}/vsFriend`}>Play a Friend</Link>
-      <Link to={`/game/${gameId}/vsComp`}>Play Computer</Link>
+      <Link to={`/game/${gameId}/${GameType.vsFriend}`}>Play a Friend</Link>
+      <br />
+      <Link to={`/game/${gameId}/${GameType.vsComputer}`}>Play Computer</Link>
     </React.Fragment>
   );
 };
