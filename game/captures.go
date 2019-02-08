20e610dbbacf53823f073a6e08ffbb94e397d261
Antonio Sviridov <ant.svirido@gmail.com>
2019-02-08 21:20:50 +0200

    done

--- game/captures.go
+++ game/captures.go
@@ -5,43 +5,83 @@ import "gomoku/board"
 func GetCaptures(b board.Board, c board.Coords) []board.Coords {
 	player := b.GetCell(c)
 	captures := []board.Coords(nil)
-	for y, row := range b {
-		for x := range row {
-			if (b.GetCell(board.Coords{x, y}) != 0) {
-				if (x+3 < 19 && b.GetCell(board.Coords{x, y}) == b.GetCell(board.Coords{x + 3, y})) {
-					if (b.GetCell(board.Coords{x + 1, y}) != 0 &&
-						b.GetCell(board.Coords{x + 1, y}) == b.GetCell(board.Coords{x + 2, y})) {
-						if b.GetCell(board.Coords{x, y}) == player {
-							captures = append(captures, board.Coords{x + 1, y}, board.Coords{x + 2, y})
-						}
-					}
-				}
-			}
-			if (y+3 < 19 && b.GetCell(board.Coords{x, y}) == b.GetCell(board.Coords{x, y + 3})) {
-				if (b.GetCell(board.Coords{x, y + 1}) != 0 &&
-					b.GetCell(board.Coords{x, y + 1}) == b.GetCell(board.Coords{x, y + 2})) {
-					if b.GetCell(board.Coords{x, y}) == player {
-						captures = append(captures, board.Coords{x, y + 1}, board.Coords{x, y + 2})
-					}
-				}
-			}
-			if (y+3 < 19 && x+3 < 19 && b.GetCell(board.Coords{x, y}) == b.GetCell(board.Coords{x + 3, y + 3})) {
-				if (b.GetCell(board.Coords{x + 1, y + 1}) != 0 &&
-					b.GetCell(board.Coords{x + 1, y + 1}) == b.GetCell(board.Coords{x + 2, y + 2})) {
-					if b.GetCell(board.Coords{x, y}) == player {
-						captures = append(captures, board.Coords{x + 1, y + 1}, board.Coords{x + 2, y + 2})
-					}
-				}
-			}
-			if (y+3 < 19 && x-3 > 1 && b.GetCell(board.Coords{x, y}) == b.GetCell(board.Coords{x - 3, y + 3})) {
-				if (b.GetCell(board.Coords{x - 1, y + 1}) != 0 &&
-					b.GetCell(board.Coords{x - 1, y + 1}) == b.GetCell(board.Coords{x - 2, y + 2})) {
-					if b.GetCell(board.Coords{x, y}) == player {
-						captures = append(captures, board.Coords{x - 1, y + 1}, board.Coords{x - 2, y + 2})
-					}
-				}
+	y := c.Y
+	x := c.X
+	if player == 0 {
+		return nil
+	}
+
+	if (x+3 < 19 && b.GetCell(board.Coords{x, y}) == b.GetCell(board.Coords{x + 3, y})) {
+		if (b.GetCell(board.Coords{x + 1, y}) != 0 &&
+			b.GetCell(board.Coords{x + 1, y}) == b.GetCell(board.Coords{x + 2, y})) {
+			if b.GetCell(board.Coords{x, y}) == player {
+				captures = append(captures, board.Coords{x + 1, y}, board.Coords{x + 2, y})
 			}
 		}
 	}
+	if (x-3 >= 0 && b.GetCell(board.Coords{x, y}) == b.GetCell(board.Coords{x - 3, y})) {
+		if (b.GetCell(board.Coords{x - 1, y}) != 0 &&
+			b.GetCell(board.Coords{x - 1, y}) == b.GetCell(board.Coords{x - 2, y})) {
+			if b.GetCell(board.Coords{x, y}) == player {
+				captures = append(captures, board.Coords{x - 1, y}, board.Coords{x - 2, y})
+			}
+		}
+	}
+
+	if (y+3 < 19 && b.GetCell(board.Coords{x, y}) == b.GetCell(board.Coords{x, y + 3})) {
+		if (b.GetCell(board.Coords{x, y + 1}) != 0 &&
+			b.GetCell(board.Coords{x, y + 1}) == b.GetCell(board.Coords{x, y + 2})) {
+			if b.GetCell(board.Coords{x, y}) == player {
+				captures = append(captures, board.Coords{x, y + 1}, board.Coords{x, y + 2})
+			}
+		}
+	}
+
+	if (y-3 >= 0 && b.GetCell(board.Coords{x, y}) == b.GetCell(board.Coords{x, y - 3})) {
+		if (b.GetCell(board.Coords{x, y - 1}) != 0 &&
+			b.GetCell(board.Coords{x, y - 1}) == b.GetCell(board.Coords{x, y - 2})) {
+			if b.GetCell(board.Coords{x, y}) == player {
+				captures = append(captures, board.Coords{x, y - 1}, board.Coords{x, y - 2})
+			}
+		}
+	}
+
+
+	if (y+3 < 19 && x+3 < 19 && b.GetCell(board.Coords{x, y}) == b.GetCell(board.Coords{x + 3, y + 3})) {
+		if (b.GetCell(board.Coords{x + 1, y + 1}) != 0 &&
+			b.GetCell(board.Coords{x + 1, y + 1}) == b.GetCell(board.Coords{x + 2, y + 2})) {
+			if b.GetCell(board.Coords{x, y}) == player {
+				captures = append(captures, board.Coords{x + 1, y + 1}, board.Coords{x + 2, y + 2})
+			}
+		}
+	}
+
+	if (y-3 >= 0 && x-3 >= 0 && b.GetCell(board.Coords{x, y}) == b.GetCell(board.Coords{x - 3, y - 3})) {
+		if (b.GetCell(board.Coords{x-1, y-1}) != 0 &&
+			b.GetCell(board.Coords{x-1, y-1}) == b.GetCell(board.Coords{x-2, y-2})) {
+			if b.GetCell(board.Coords{x, y}) == player {
+				captures = append(captures, board.Coords{x-1, y-1}, board.Coords{x-2, y-2})
+			}
+		}
+	}
+
+	if (y+3 < 19 && x-3 >= 0 && b.GetCell(board.Coords{x, y}) == b.GetCell(board.Coords{x - 3, y + 3})) {
+		if (b.GetCell(board.Coords{x - 1, y + 1}) != 0 &&
+			b.GetCell(board.Coords{x - 1, y + 1}) == b.GetCell(board.Coords{x - 2, y + 2})) {
+			if b.GetCell(board.Coords{x, y}) == player {
+				captures = append(captures, board.Coords{x - 1, y + 1}, board.Coords{x - 2, y + 2})
+			}
+		}
+	}
+
+	if (y-3 >= 0 && x+3 < 19 && b.GetCell(board.Coords{x, y}) == b.GetCell(board.Coords{x + 3, y - 3})) {
+		if (b.GetCell(board.Coords{x + 1, y - 1}) != 0 &&
+			b.GetCell(board.Coords{x + 1, y - 1}) == b.GetCell(board.Coords{x + 2, y - 2})) {
+			if b.GetCell(board.Coords{x, y}) == player {
+				captures = append(captures, board.Coords{x + 1, y - 1}, board.Coords{x + 2, y - 2})
+			}
+		}
+	}
+
 	return captures
 }
