c6f509b44e51ab54040a7479a79c97baa6dffac5
Alexander Pashkov <alexpashkov123@gmail.com>
2019-02-10 19:38:45 +0200

    Add IsTerminate test

---
+++ minimax/heuristic/is_terminate_test.go
@@ -0,0 +1,33 @@
+package heuristic
+
+import (
+	"testing"
+	"gomoku/board"
+)
+
+var IsTerminateValidTestCases = map[string]struct {
+	board.Board
+	blackScore, whiteScore, expected int8
+}{
+	"score black": {
+		Board:      board.Board{},
+		blackScore: 12,
+		whiteScore: 0,
+		expected:   board.BLACK_PLAYER,
+	},
+	"score white": {
+		Board:      board.Board{},
+		blackScore: 12,
+		whiteScore: 0,
+		expected:   board.WHITE_PLAYER,
+	},
+}
+
+func TestIsTerminate(t *testing.T) {
+	for descr, testCase := range IsTerminateValidTestCases {
+		received := IsTerminate(testCase.Board, testCase.blackScore, testCase.whiteScore)
+		if received != testCase.expected {
+			t.Errorf("%s, got: %d, expected: %d", descr, received, testCase.expected)
+		}
+	}
+}
