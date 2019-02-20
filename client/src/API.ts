4e00281e6e4127d59546be17d695d45750e5c3b8
Alex Pashkov <alexpashkov123@gmail.com>
2019-02-20 18:06:09 +0200

    Refactor API

--- client/src/API.ts
+++ client/src/API.ts
@@ -1,7 +1,9 @@
 import axios from "axios";
 import { ICommonGameStateWithBoard, ICoords, ISuggestion } from "./types";
 
-const BASE_URL = process.env.REACT_APP_BASE_URL;
+const API = axios.create({
+  baseURL: process.env.REACT_APP_BASE_URL || ""
+});
 
 export function makeMove(
   state: ICommonGameStateWithBoard,
@@ -9,19 +11,23 @@ export function makeMove(
 ): Promise<ICommonGameStateWithBoard> {
   const cell = state.board[coords.y][coords.x];
   if (cell) return Promise.reject("cell is occupied");
-  return axios
-    .post(BASE_URL + "/make-move", {
-      state,
-      coords
-    })
-    .then(({ data }) => data, ({ response }) => Promise.reject(response.data));
+  return API.post("/make-move", {
+    state,
+    coords
+  }).then(
+    ({ data }) => data,
+    ({ response }) =>
+      Promise.reject(response ? response.data : "failed request")
+  );
 }
 
 export function suggestMoves(
   state: ICommonGameStateWithBoard,
   difficulty: number
 ): Promise<ISuggestion[]> {
-  return axios
-    .post(`${BASE_URL}/suggest-moves?difficulty=${difficulty}`, state)
-    .then(({ data }) => data, ({ response }) => Promise.reject(response.data));
+  return API.post(`/suggest-moves?difficulty=${difficulty}`, state).then(
+    ({ data }) => data,
+    ({ response }) =>
+      Promise.reject(response ? response.data : "failed request")
+  );
 }
